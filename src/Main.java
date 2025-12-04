a1bcc4d1cddedafef562c4a4291b58c580d89c1b
Jimmy <crosk2000@hotmail.com>
2025-12-04 10:04:14 +0100

    Codigo popoCode

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner KB = new Scanner(System.in);
+        Random RNG = new Random();
 
         System.out.print("Cuantos popochos tienes? ");
         int popocho;
@@ -25,5 +27,24 @@ public class Main {
                 System.out.println("Demasiados popochos, no habra espacio en la cama");
                 break;
         }
+
+        int[] popoCode = new int[popocho];
+        int max = 101;
+        boolean check;
+
+        for(int i=0; i<popocho; i++){
+            popoCode[i] = RNG.nextInt(max);
+            for(int c=0; c<popocho; c++){
+                check = false;
+                if(i!=c && popoCode[i]==popoCode[c]){
+                    popoCode[i] = RNG.nextInt(max);
+                    check = true;
+                }
+                if(check)
+                    c = 0;
+            }
+            System.out.println("Popocho nº"+i+" - Codigo: "+popoCode[i]);
+        }
+
     }
 }
